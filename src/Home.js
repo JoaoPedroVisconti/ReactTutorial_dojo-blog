d377e5f7e10e4e8c43e9c93b2a582cd7c571808c
JoaoPedroVisconti <visconti.joao@gmail.com>
2021-06-09 16:59:39 +0100

    Output a list of states (objects type)

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-  const [name, setName] = useState('Mario');
-  const [age, setAge] = useState(31);
-
-  const handleClick = () => {
-    setName('Luigi');
-    setAge(32);
-  };
-
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party', body: 'lorem ipsum...', author: 'luigi', id: 2 },
+    { title: 'Web dev top', body: 'lorem ipsum...', author: 'yoshi', id: 3 },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
