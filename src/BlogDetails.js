8527173f0af2fc04ea75b034908a23da513f0df4
JoaoPedroVisconti <visconti.joao@gmail.com>
2021-06-11 15:57:24 +0100

    Using route parameters to set a link to page blogs details and turning the blogs on the home page links

---
+++ src/BlogDetails.js
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom';
+
+const BlogDetails = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="blog-details">
+      <h2>{id} - Blog Details</h2>
+    </div>
+  );
+};
+
+export default BlogDetails;
