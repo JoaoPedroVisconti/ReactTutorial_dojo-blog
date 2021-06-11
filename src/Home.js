c1e36af268a537702f3dd6d6eb9d5010950465df
JoaoPedroVisconti <visconti.joao@gmail.com>
2021-06-11 10:32:36 +0100

    Trigger useEffect with dependency

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Web dev top', body: 'lorem ipsum...', author: 'yoshi', id: 3 },
   ]);
 
+  const [name, setName] = useState('Mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
 
@@ -16,12 +18,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log('useEffect ran');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>Change Name</button>
+      <p>{name}</p>
     </div>
   );
 };
